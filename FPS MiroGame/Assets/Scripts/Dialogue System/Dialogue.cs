f70c86aa91693e2ec4aa5579e1f06ffa09c4f65c
Devon Nutting <36216700+DA-Nutt01@users.noreply.github.com>
2025-02-28 17:50:03 -0800

    Dialogue Scriptable Objects

---
+++ FPS MiroGame/Assets/Scripts/Dialogue System/Dialogue.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+public class Dialogue : ScriptableObject
+{
+    [SerializeField] DialogueNode[] _nodes; // An array of the DialogueNode scriptable objects
+
+    
+}
+
