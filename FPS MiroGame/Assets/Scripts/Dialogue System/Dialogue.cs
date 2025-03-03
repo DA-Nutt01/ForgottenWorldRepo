0ba66954266208655b444eb752796bf6d33cb7d8
Devon Nutting <36216700+DA-Nutt01@users.noreply.github.com>
2025-03-02 20:46:57 -0800

    Reapply "Dialogue Editor Updates"
    This reverts commit e6c08e3d09fb3845cb42e4725b0ee00b5e800a38.

--- FPS MiroGame/Assets/Scripts/Dialogue System/Dialogue.cs
+++ FPS MiroGame/Assets/Scripts/Dialogue System/Dialogue.cs
@@ -7,8 +7,24 @@ namespace Dialogue
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
     public class Dialogue : ScriptableObject
     {
-        [SerializeField] DialogueNode[] _nodes; // An array of the DialogueNode scriptable objects
+        [SerializeField] 
+        List<DialogueNode> _nodes; // A list of the DialogueNode scriptable objects
 
-
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            // Create a root dialogue node for this scriptable object when it is created if it does not already have one
+            if (_nodes.Count == 0)
+            {
+                _nodes.Add(new DialogueNode());
+            }
+        }
+#endif
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            // A getter function to return all nodes in this scriptable object
+            return _nodes;
+        }
     }
+
 }
